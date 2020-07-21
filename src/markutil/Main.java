59a124201ee9152e82de29eb34cc1d5cf50e79ff
Long Phạm <longptse140136@fpt.edu.vn>
2020-07-21 08:02:04 +0700

    3rd: add JUnit class and .jar files

--- src/markutil/Main.java
+++ src/markutil/Main.java
@@ -18,6 +18,44 @@ public class Main {
      */
     public static void main(String[] args) {
         System.out.println("5! = " + computeFactorial(5));
+        
+        //Expected: hi vọng đưa về  720 khi đưa số 6
+        System.out.println("6! = " + computeFactorial(6));
+        //Expected: hi vọng đưa về  1 khi đưa số 1
+        System.out.println("1! = " + computeFactorial(1));
+        //Expected: hi vọng đưa về  1 khi đưa số 0
+        System.out.println("0! = " + computeFactorial(0));
+        //Expected: hi vọng đưa về  ngoại lệ khi đưa số -5
+        System.out.println("-5! = " + computeFactorial(-5));
+        // hello test change 
+        //Để test phần mềm ta làm việc sau
+        //1. Chuẩn bị data ví dụ 5, 6, 7, 1, 2,...
+        //2. Tính toán bằng tay trước xem 5! là bằng mấy, 6! là bằng mấy
+        //ví dụ tính 6! là 720, 720 gọi là giá trị kì vọng EXPECTED VALUE
+        
+        //3. Gọi lệnh, gọi app, gọi hàm chạy thử với data đã chuẩn bị
+        //coi xem hàm trả về số mấy kết quả mấy,
+        // ví dụ khi chạy hàm cF(6) mà nó trả về 120, thì giá trị trả về
+        // khi chạy hàm gọi là ACTUAL VALUE
+        
+        //4. cái hồi mình phải so sánh xem EXPECTED và ACTUAL có trùng nhau hay ko
+        // nếu trùng: tức là hàm xử lí ra kết quả như mình dự kiến -> hàm chạy đúng
+        // nếu ko trùng : mình tính sai, hay hàm tính sai , nói chung là sai
+        
+        //TEST: BẢN CHẤT LÀ CHẠY THỬ APP/HÀM, SO SÁNH KẾT QUẢ KÌ VỌNG CÓ TRƯỚC
+        //VỚI KẾT QUẢ HÀM THỰC SỰ XỬ LÍ VÀ TRẢ VỀ
+        
+        //Có cách viết đơn giản nhưng chưa hay
+        //Mình phải tự so sánh bằng mắt giá trị tự trả về của hàm và kì vọng 
+        //Nó ko cấm mình disable nút Clean and Build -> code sai logic mà vẫn ra được file jar.war
+        //Ta cần cách khác để đảm bảo chất lượng code ngay khi viết code 
+        //ta cần dùng thêm thư viện hỗ trợ để kiểm tra chất lượng của hàm 
+        //đó là kĩ thuật TDD kèm UNIT TESTING
+        // xài thư viện JUnit, NUnit, TestNG, xUnit, MsUnit,...
+        //những thưu viện này ko bắt mình phải so sánh bằng mắt các ACTUAL vs EXPECTED
+        //nó tự so sánh giùm và nó QUĂNG VỀ 2 THỨC: XANH, ĐỎ
+        //XANH: CODE ỔN
+        //ĐỎ: CODE SAI LOGIN 
     }
 
 }
---
+++ test/tdd/MarkUtilTest.java
@@ -0,0 +1,56 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tdd;
+
+import longpt.util.MarkUtil;
+import org.junit.Assert;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author phamt
+ */
+public class MarkUtilTest {
+
+    @Test
+    public void testSuccessfulCases() {
+        //hàm này test những tình huống thành công tức là đưa đầu vào tử tế cho hàm cF() (tính giai thừa)
+        Assert.assertEquals(55, 55);
+        Assert.assertEquals(1, MarkUtil.computeFactorial(1));
+        Assert.assertEquals(2, MarkUtil.computeFactorial(2));
+        Assert.assertEquals(6, MarkUtil.computeFactorial(3));
+        Assert.assertEquals(24, MarkUtil.computeFactorial(4));
+        Assert.assertEquals(120, MarkUtil.computeFactorial(5));
+        
+    }
+
+    //@Test biến hàm thành 1 hàm main() riêng biệt và Shift F6 chạy
+    //cmt @Test phế hàm này thành hàm bt, ko được chạy trong lần Shift F6
+    //trò này do thư viện JUnit nó bày ra, bày thêm màu sắc XANH ĐỎ VÀNG
+    
+    @Test(expected = IllegalArgumentException.class)
+    public void testFailCases() {
+        //hàm này test những tình huống cà chớn và đưa đầu vào cà cớn
+        //ví dụ cF(-1) sao mà tính được và cF(20) to quá
+        //Ngoại lệ là 1 thứ không thể so sánh
+        //Exception là 1 tình huống ko dữ liệu đc, do đó ko có value
+        //do đó kko thể so sánh được nên ko dùng hàm assertEquals được 
+        //vì nó so sánh value theo màu săsc
+        //vậy ta phải xài chiêu khác khi chơi với ngoại lệ
+        // kì vọng rằng khi đưa ra cF(-5) thì hàm cF() này sẽ ném ra cái ngoại lệ 
+        //thông báo ra tham số cà chớn
+        //câu này viết thế nào trong code để ra màu xanh hay đỏ
+        MarkUtil.computeFactorial(-5); //ra màu đỏ vì nó ko ra lỗi như expected trên line 35
+    }
+}
+
+//dù code bạn sai logic nhưng bạn vẫn ra đc file .jar, .war khi Clean & Build
+//nguy hiểm, ko tốt 
+//ta sẽ chơi trò disable nút Clean & Build nếu code còn đang màu đỏ
+//chơi với JUnit thì làm được trò này
+//chơi truyền thống sout() JOptionPane, ghi ra file log môn JavaWeb thì kiểu nào cũng ra được file .jar, .war nếu ko sai cú pháp
+//hãy nhớ 2 con số nếu xài NB 8.2 1005, NB 10 trở lên,
