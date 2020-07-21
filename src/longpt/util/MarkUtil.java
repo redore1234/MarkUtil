4526bc2dbc4e8254f9775c2b2d56b2e668eb8384
Long Phạm <longptse140136@fpt.edu.vn>
2020-07-21 09:51:29 +0700

    5rd: replace algorithm by recursion

--- src/longpt/util/MarkUtil.java
+++ src/longpt/util/MarkUtil.java
@@ -9,22 +9,27 @@ package longpt.util;
  *
  * @author phamt
  */
-
 //class này chứa các hàm tiện ích hỗ trợ phép tính toán 
 //vì nó là tiện ích hàm ý dùng chung cho mọi nơi
 //do đó chúng nó là static
 public class MarkUtil {
+
     //hàm giai thừa n! = 1, 2, 3,..., n bùng nổ kết quả rất nhanh
     //nên tràn int sớm,  ta trả về long cỡ 15!
     public static long computeFactorial(int n) {
         //ta ko tính giai thừa âm, và n > 15, vì quá lớn, tràn số
-        if (n<0 || n>15) {
+        if (n < 0 || n > 15) {
             throw new IllegalArgumentException("Invalid argument, n<0 and n>15");
         }
-        long result = 1;
-        for (int i = 1; i <= n; i++) {
-            result *= i;
+
+        if (n == 0 || n == 1) {
+            return 1; //điểm dừng của giai thừa
         }
-        return result;
+        return n * computeFactorial(n - 1);
+//        long result = 1;
+//        for (int i = 1; i <= n; i++) {
+//            result *= i;
+//        }
+//        return result;
+        }
     }
-}
